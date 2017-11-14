d3de78cf9a002221d07e60d3ebf6ead41ab4a0fa
Tilo Germer <tilo.germer@t-online.de>
2017-11-14 14:01:12 +0100

    added lent books to about page in header

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,11 +1,13 @@
 class AboutController < ApplicationController
 
 	def index
-		@books = Book.all
+		@books = Book.where(:isReturned => false).order("return_date ASC")
 		@categories = Category.all
 		@category = Category.new
 		@libraries = Library.all
 		@readers = Reader.all
+		
+		
 		
 	end
 end
