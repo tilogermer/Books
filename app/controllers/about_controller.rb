cfccd85dd81c5067a935bb41932ae7ccbdb15584
Tilo Germer <tilo.germer@t-online.de>
2017-11-15 14:01:33 +0100

    added readers images

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -5,7 +5,11 @@ class AboutController < ApplicationController
 		@books3 = Book.where(:isReturned => false).minimum(:return_date)
 
 		@books1 = Book.where(:isFavorite => true)
-		@books2 = Book.where(:isReturned => false, :library_id =>2).order("return_date ASC")
+		@books2 = Book.where(:isReturned => false, :library_id =>2)
+		@books4 = Book.where(:isReturned => false, :library_id =>1)
+		@books6 = Book.where(:isReturned => false, :library_id =>2).minimum(:return_date)
+		@books5 = Book.where(:isReturned => false, :library_id =>1).minimum(:return_date)
+
 		
 
 		@categories = Category.all
