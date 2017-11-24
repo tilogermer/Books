f729af79d4c1e4494c9e8132a6642cafa2a29699
Tilo Germer <tilo.germer@t-online.de>
2017-11-24 14:22:48 +0100

    improved card design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+	def remove_unwanted_words string
+  bad_words = ["less than", "about"]
+
+  bad_words.each do |bad|
+    string.gsub!(bad + " ", '')
+  end
+
+  return string
+end
 end
